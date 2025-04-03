5dcff474502c9dd304907c39d9d006f71c90689d
Dylan101102 <136282326+Dylan101102@users.noreply.github.com>
2025-04-02 21:56:56 -0400

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/profile.jpeg", width=600)
+
+with col2:
+    st.title("Dylan Roach")
+    content = """
+    Hi there, my name is Dylan. I'm an FSU graduate with a bachelor's in meteorology and minor in programming. Currently, I work for
+    the state of Florida as a Systems Project Consultant with the Department of Health. In my spare time, 
+    I generally like to learn programming (currently learning Python) and hang out with my friends.
+    """
+    st.info(content)
