953e43ddb556bfaea641a77be0f93e4d92f1b642
Dylan Roach <dylan.roach@flhealth.gov>
2025-04-03 16:16:18 -0400

    Add description on the new columns

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ with col2:
     I generally like to learn programming (currently learning Python) and hang out with my friends.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
